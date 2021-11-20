c38f87bab681cee273b94a52f83e07044c05fef8
tomdziwood <tomdziwood@gmail.com>
2021-11-20 18:25:11 +0100

    Weryfikowanie poprawności uszeregowania jsq

--- skrypty_techniczne/weryfikator_poprawnosci_uszeregowania.py
+++ skrypty_techniczne/weryfikator_poprawnosci_uszeregowania.py
@@ -64,7 +64,101 @@ def weryfikuj_uszeregowanie_jnq(nazwa_pliku_wejsciowego, nazwa_pliku_wyjsciowego
 
 def weryfikuj_uszeregowanie_jsq(nazwa_pliku_wejsciowego, nazwa_pliku_wyjsciowego):
     print("Weryfikacja uszeregowania jsq: " + nazwa_pliku_wyjsciowego + "\t(" + nazwa_pliku_wejsciowego + ")")
-    return [0, nazwa_pliku_wyjsciowego]
+
+    zadania = []
+    f = open(file=nazwa_pliku_wejsciowego, mode="r")
+    for line in f:
+        line_split = line.split(' ')
+        zadania.append([int(line_split[0]), int(line_split[1])])
+    f.close()
+
+    f = open(file=nazwa_pliku_wyjsciowego, mode="r")
+    [sredni_czas_opoznienia, sredni_czas_przetwarzania, sredni_czas_odpowiedzi] = [float(liczba) for liczba in f.readline().split(' ')]
+    wezly = []
+    for line in f:
+        lista_zadan = [int(indeks_zadania) for indeks_zadania in line.split()]
+        lista_zadan.pop(0)
+        wezly.append(lista_zadan)
+    f.close()
+
+    wzorzec_liczby_wezlow = re.compile('^\\.\\./uszeregowanie/szer-.*-n([0-9]*)-nodes-.*\\.txt$')
+    liczba_wezlow = int(wzorzec_liczby_wezlow.match(nazwa_pliku_wyjsciowego).group(1))
+
+    czas_opoznienia = []
+    czas_przetwarzania = []
+    czas_odpowiedzi = []
+
+    for lista_zadan in wezly:
+        czas_wezla = 0
+        wykonywane_zadania_wezla = []
+        for indeks_zadania in lista_zadan:
+            zadanie = zadania[indeks_zadania]
+            moment_gotowosci = int(zadanie[0])
+            rozmiar_zadania = int(zadanie[1])
+
+            liczba_wykonywanych_zadan = len(wykonywane_zadania_wezla)
+            while (liczba_wykonywanych_zadan > 0) and (czas_wezla + wykonywane_zadania_wezla[0][1] <= moment_gotowosci):
+                czas_ukonczenia = czas_wezla + wykonywane_zadania_wezla[0][1]
+                czas_opoznienia.append(0)
+                czas_przetwarzania.append(czas_ukonczenia - wykonywane_zadania_wezla[0][0])
+                czas_odpowiedzi.append(czas_przetwarzania[-1])
+
+                uplyniety_czas = wykonywane_zadania_wezla[0][1]
+                wykonywane_zadania_wezla.pop(0)
+                liczba_wykonywanych_zadan -= 1
+                czas_wezla = czas_ukonczenia
+
+                j = 0
+                while j < liczba_wykonywanych_zadan:
+                    wykonywane_zadanie = wykonywane_zadania_wezla[j]
+                    wykonywane_zadanie[1] = (wykonywane_zadanie[1] - uplyniety_czas) * liczba_wykonywanych_zadan / (liczba_wykonywanych_zadan + 1)
+                    j += 1
+
+            i = 0
+            uplyniety_czas = moment_gotowosci - czas_wezla
+            while i < liczba_wykonywanych_zadan:
+                wykonywane_zadania_wezla[i][1] = (wykonywane_zadania_wezla[i][1] - uplyniety_czas) * (liczba_wykonywanych_zadan + 1) / liczba_wykonywanych_zadan
+                i += 1
+
+            czas_wezla = moment_gotowosci
+
+            i = 0
+            czas_trwania_nowego_zadania = rozmiar_zadania * liczba_wezlow * (liczba_wykonywanych_zadan + 1)
+            while i < liczba_wykonywanych_zadan:
+                if czas_trwania_nowego_zadania < wykonywane_zadania_wezla[i][1]:
+                    break
+                i += 1
+            wykonywane_zadania_wezla.insert(i, [moment_gotowosci, czas_trwania_nowego_zadania])
+
+        liczba_wykonywanych_zadan = len(wykonywane_zadania_wezla)
+        while liczba_wykonywanych_zadan > 0:
+            czas_ukonczenia = czas_wezla + wykonywane_zadania_wezla[0][1]
+            czas_opoznienia.append(0)
+            czas_przetwarzania.append(czas_ukonczenia - wykonywane_zadania_wezla[0][0])
+            czas_odpowiedzi.append(czas_przetwarzania[-1])
+
+            uplyniety_czas = wykonywane_zadania_wezla[0][1]
+            wykonywane_zadania_wezla.pop(0)
+            liczba_wykonywanych_zadan -= 1
+            czas_wezla = czas_ukonczenia
+
+            j = 0
+            while j < liczba_wykonywanych_zadan:
+                wykonywane_zadanie = wykonywane_zadania_wezla[j]
+                wykonywane_zadanie[1] = (wykonywane_zadanie[1] - uplyniety_czas) * liczba_wykonywanych_zadan / (liczba_wykonywanych_zadan + 1)
+                j += 1
+
+    obliczony_sredni_czas_opoznienia = sum(czas_opoznienia) / len(czas_opoznienia)
+    obliczony_sredni_czas_przetwarzania = sum(czas_przetwarzania) / len(czas_przetwarzania)
+    obliczony_sredni_czas_odpowiedzi = sum(czas_odpowiedzi) / len(czas_odpowiedzi)
+
+    if (sredni_czas_opoznienia == obliczony_sredni_czas_opoznienia) and (sredni_czas_przetwarzania == obliczony_sredni_czas_przetwarzania) and (sredni_czas_odpowiedzi == obliczony_sredni_czas_odpowiedzi):
+        return [1]
+    else:
+        print("[ERROR]\tNowe obliczone srednie statystyki maja rozne wartosci od podanych przez program szeregujacy.")
+        print("sredni_czas_opoznienia=\t\t\t\t" + str(sredni_czas_opoznienia) + "\tsredni_czas_przetwarzania=\t\t\t\t" + str(sredni_czas_przetwarzania) + "\tsredni_czas_odpowiedzi=\t\t\t\t" + str(sredni_czas_odpowiedzi))
+        print("obliczony_sredni_czas_opoznienia=\t" + str(obliczony_sredni_czas_opoznienia) + "\tobliczony_sredni_czas_przetwarzania=\t" + str(obliczony_sredni_czas_przetwarzania) + "\tobliczony_sredni_czas_odpowiedzi=\t" + str(obliczony_sredni_czas_odpowiedzi))
+        return [-1, nazwa_pliku_wyjsciowego]
 
 
 def weryfikuj_uszeregowanie_test(nazwa_pliku_wejsciowego, nazwa_pliku_wyjsciowego):
@@ -215,6 +309,19 @@ def weryfikuj_uszeregowanie_proba_02():
     wyniki_weryfikacji.wyswietl_wyniki_weryfikacji()
 
 
+def weryfikuj_uszeregowanie_proba_03():
+    wyniki_weryfikacji = WynikWeryfikacjiWszystkichUszeregowan()
+    liczby_wezlow = [1, 2, 5, 10, 20, 50, 100]
+
+    for liczba_wezlow in liczby_wezlow:
+        nazwa_pliku_wejsciowego = "../instancje/inst-przedk-1.75-c020.txt"
+        nazwa_pliku_wyjsciowego = "../uszeregowanie/szer-jsq-n" + "%03d-nodes-" % liczba_wezlow + "inst-przedk-1.75-c020.txt"
+        wynik_weryfikacji = weryfikuj_uszeregowanie(nazwa_pliku_wejsciowego, nazwa_pliku_wyjsciowego)
+        wyniki_weryfikacji.dodaj_wynik_weryfikacji(wynik_weryfikacji)
+
+    wyniki_weryfikacji.wyswietl_wyniki_weryfikacji()
+
+
 def weryfikuj_wszystkie_uszeregowania_wszystkich_instancji():
     zawartosc_folderu_instancje = os.listdir("../instancje")
     wzorzec_inst = re.compile('^inst.*\\.txt$')
@@ -272,10 +379,11 @@ class WynikWeryfikacjiWszystkichUszeregowan:
 
 
 def main():
-    weryfikuj_wszystkie_uszeregowania_wszystkich_instancji()
-
     # weryfikuj_uszeregowanie_proba_01()
     # weryfikuj_uszeregowanie_proba_02()
+    # weryfikuj_uszeregowanie_proba_03()
+
+    weryfikuj_wszystkie_uszeregowania_wszystkich_instancji()
 
 
 if __name__ == "__main__":
